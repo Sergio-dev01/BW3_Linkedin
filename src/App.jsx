1d949fea734c028be58cc7a2c3e769b17d3d5fa2
Christian Ragonese <christianragonese1313@gmail.com>
2025-06-03 17:37:15 +0200

    adds

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import TopBar from "./components/TopBar";
 import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <TopBar />
       <MainContent />
+      <Footer />
     </>
   );
 }
