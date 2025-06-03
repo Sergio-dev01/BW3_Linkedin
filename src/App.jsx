0e6a91d4dabad98e9a2069a1dad2cb2fdc3d782f
Sergio-dev01 <sergio.develoop@gmail.com>
2025-06-03 17:24:18 +0200

    add MainContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import TopBar from "./components/TopBar";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <>
       <TopBar />
+      <MainContent />
     </>
   );
 }
